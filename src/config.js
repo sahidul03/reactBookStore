624ae91b52e8cd6ad1a6963864917fccb894adda
Sahidul Islam <sahidul.islam@bjitgroup.com>
2018-01-04 16:58:33 +0600

    added a config file to react project keep all settings to this file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
+let domain = 'http://localhost';
 let config = {
-    "frontendBaseUrl": 'http://localhost:3000',
-    "backendBaseUrl": 'http://localhost:5000'
+    "frontendBaseUrl": domain + ':3000',
+    "backendBaseUrl": domain + ':5000'
 };
 export default config;
